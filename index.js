f03ef26bd3e1e7c993ab6c37447a416c669c7af5
Laura-Cos3 <cauraloombes@gmail.com>
2025-02-06 21:32:24 +0000

    Update index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let losAngelesElement= document.queryselector("#los-angeles");
+let losAngelesDateElement= losAngelesElement.querySelector(".date");
+let losAngelesTimeElement= losAngelesElement.querySelector(".time");
+losAngelesDateElement.innerHTML= "February 07 2025"
+losAngelesTimeElement.innerHTML= "9:25:48 <small>PM</small>";
