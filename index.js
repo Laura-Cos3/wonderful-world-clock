2211db913cff41c3cf5e28a400004eab9c7c1483
Laura-Cos3 <cauraloombes@gmail.com>
2025-02-06 21:43:37 +0000

    Update index.js
    Add time and date

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let losAngelesElement= document.queryselector("#los-angeles");
 let losAngelesDateElement= losAngelesElement.querySelector(".date");
 let losAngelesTimeElement= losAngelesElement.querySelector(".time");
-losAngelesDateElement.innerHTML= "February 07 2025"
-losAngelesTimeElement.innerHTML= "9:25:48 <small>PM</small>";
+losAngelesDateElement.innerHTML= moment().format("Do MMMM YYYY");
+losAngelesTimeElement.innerHTML= moment().format("hh:mm:ss a");
