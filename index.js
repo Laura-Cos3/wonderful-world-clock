f5d574a33946761bcf658d21b68ab3240a16b083
Laura-Cos3 <cauraloombes@gmail.com>
2025-02-06 22:18:30 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 let losAngelesElement= document.queryselector("#los-angeles");
 let losAngelesDateElement= losAngelesElement.querySelector(".date");
 let losAngelesTimeElement= losAngelesElement.querySelector(".time");
-let losAngelesTime= moment();
+let losAngelesTime= moment().tz("America/Los_Angeles);
 
 losAngelesDateElement.innerHTML= losAngelesTime.format("Do MMMM YYYY");
 losAngelesTimeElement.innerHTML= `${losAngelesTime.format("h:mm:ss") <small>${losAngelesTime.format("A")}</small>`;
+
+let sydneyElement= document.queryselector("#sydney");
+let sydneyDateElement= sydneyElement.querySelector(".date");
+let sydneyTimeElement= sydneyElement.querySelector(".time");
+let sydneyTime= moment().tz("Australia/Sydney);
+
+sydneyDateElement.innerHTML= sydneyTime.format("Do MMMM YYYY");
+sydneyTimeElement.innerHTML= `${sydneyTime.format("h:mm:ss") <small>${sydneyTime.format("A")}</small>`;
+
